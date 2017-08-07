f05c3002d5e511442228c72adb77b7a4d6495f25
Jack Pettit <jpettit@jacks-mbp>
2017-08-06 23:30:29 -0500

    Added IBActions and IBOutlets to AddItemViewController.

--- MyInventory/MyInventory/AddItemViewController.swift
+++ MyInventory/MyInventory/AddItemViewController.swift
@@ -11,7 +11,17 @@ import UIKit
 
 class AddItemViewController: UIViewController {
     
+    @IBOutlet weak var nameField: UITextField!
+    @IBOutlet weak var quantityField: UITextField!
+    @IBOutlet weak var ownerField: UITextField!
+    
+    @IBOutlet weak var addItem: UIButton!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+    }
+    @IBAction func addItemPressed(_ sender: UIButton) {
+        
     }
 }
