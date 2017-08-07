008d381769a0b9b407cb70786ff7a90aa2763a4a
Jack Pettit <jpettit@jacks-mbp>
2017-08-06 23:44:10 -0500

    Textfield now resigns when background is tapped or return key is pressed.

--- MyInventory/MyInventory/AddItemViewController.swift
+++ MyInventory/MyInventory/AddItemViewController.swift
@@ -9,19 +9,40 @@
 import Foundation
 import UIKit
 
-class AddItemViewController: UIViewController {
-    
+class AddItemViewController: UIViewController, UITextFieldDelegate {
     @IBOutlet weak var nameField: UITextField!
     @IBOutlet weak var quantityField: UITextField!
     @IBOutlet weak var ownerField: UITextField!
     
     @IBOutlet weak var addItem: UIButton!
     
+    private var newItem: Item = Item()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-    }
-    @IBAction func addItemPressed(_ sender: UIButton) {
+        addItem.isEnabled = false
         
+        // set textfield delegates
+        nameField.delegate = self
+        quantityField.delegate = self
+        ownerField.delegate = self
+        
+        // set tap gesture recognizer
+        let tapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(backgroundTapped))
+        view.addGestureRecognizer(tapGestureRecognizer)
+    
+    }
+    
+    func backgroundTapped() {
+        view.endEditing(true)
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        view.endEditing(true)
+        return true
+    }
+    
+    @IBAction func addItemPressed(_ sender: UIButton) {
+        print("Add Item pressed.")
     }
 }
