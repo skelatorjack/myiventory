2810c936f2638a1b93678839b47cb356c3ca44c2
Jack Pettit <jpettit@jacks-mbp>
2017-08-21 19:28:18 -0500

    Created AddShoppingListViewController

---
+++ MyInventory/MyInventory/AddShoppingListViewController.swift
@@ -0,0 +1,24 @@
+//
+//  AddShoppingListViewController.swift
+//  MyInventory
+//
+//  Created by Jack Pettit on 8/21/17.
+//  Copyright © 2017 jpettit. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class AddShoppingListViewController: UIViewController {
+    
+    @IBOutlet weak var listNameTextField: UITextField!
+    @IBOutlet weak var addShoppingListButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    @IBAction func addShoppingListPressed(_ sender: UIButton) {
+    }
+    
+}
