f20acf1b190977be1cc828038fe23a7e01dfa386
Jack Pettit <jpettit@jacks-mbp>
2017-08-21 22:03:09 -0500

    Added ShoppingListDetailViewController

---
+++ MyInventory/MyInventory/ShoppingListDetailViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ShoppingListDetailViewController.swift
+//  MyInventory
+//
+//  Created by Jack Pettit on 8/21/17.
+//  Copyright © 2017 jpettit. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class ShoppingListDetailViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
