240623951411c433b56a934792ce9f45094bb71a
Jack Pettit <jpettit@Jacks-MacBook-Pro.local>
2017-08-24 13:05:09 -0500

    Added Extensions.swift

---
+++ MyInventory/MyInventory/Extensions.swift
@@ -0,0 +1,17 @@
+//
+//  Extensions.swift
+//  MyInventory
+//
+//  Created by Jack Pettit on 8/24/17.
+//  Copyright © 2017 jpettit. All rights reserved.
+//
+
+import Foundation
+
+/*
+extension String {
+    static func == (left: String, right: String) -> Bool {
+        return left == right
+    }
+}
+ */
