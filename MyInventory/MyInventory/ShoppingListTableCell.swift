cb732d802ba7bf76c1a6aeb638e1f1803d1a283c
Jack Pettit <skelatorjack@gmail.com>
2017-12-26 18:37:59 -0600

    The shoppingListCountLabel now gets the number of items from getTotalNumberOfItemsInList.

--- MyInventory/MyInventory/ShoppingListTableCell.swift
+++ MyInventory/MyInventory/ShoppingListTableCell.swift
@@ -16,6 +16,6 @@ class ShoppingListTableCell: UITableViewCell {
     
     func decorate(with shoppingList: ShoppingList) {
         shoppingListNameLabel.text = shoppingList.getListName()
-        shoppingListCountLabel.text = "\(shoppingList.getTotalNumberOfItems())"
+        shoppingListCountLabel.text = "\(shoppingList.getTotalNumberOfItemsInList())"
     }
 }
