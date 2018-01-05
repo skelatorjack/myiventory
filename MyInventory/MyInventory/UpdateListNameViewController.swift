edbfb4a6a5a50acbb0556d3af8b7993f722e5fd2
Jack Pettit <skelatorjack@gmail.com>
2018-01-04 22:25:17 -0600

    Added UpdateListNameViewController.

---
+++ MyInventory/MyInventory/UpdateListNameViewController.swift
@@ -0,0 +1,17 @@
+//
+//  UpdateListNameViewController.swift
+//  MyInventory
+//
+//  Created by Jack Pettit on 1/4/18.
+//  Copyright © 2018 jpettit. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class UpdateListNameViewController: UIViewController, UITextFieldDelegate {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
