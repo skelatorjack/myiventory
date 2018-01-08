45c22105668c41ff1e80c5037583f46230189eea
Jack Pettit <skelatorjack@gmail.com>
2018-01-07 21:50:17 -0600

    Implemented picker for shopping lists.

--- MyInventory/MyInventory/ChangeItemShoppingListViewController.swift
+++ MyInventory/MyInventory/ChangeItemShoppingListViewController.swift
@@ -11,9 +11,40 @@
 import Foundation
 import UIKit
 
-class ChangeItemShoppingListViewController: UIViewController {
+class ChangeItemShoppingListViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
+    
+    @IBOutlet weak var changeShoppingListButton: UIButton!
+    @IBOutlet weak var shoppingListPicker: UIPickerView!
+    
+    private var listOfShoppingListNames: [String] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        shoppingListPicker.delegate = self
+        shoppingListPicker.dataSource = self
     }
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return listOfShoppingListNames.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return listOfShoppingListNames[row]
+    }
+    
+    func setListOfShoppingListNames(newList: [String] = []) {
+        listOfShoppingListNames = newList
+    }
+    
+    func getListOfShoppingListNames() -> [String] {
+        return listOfShoppingListNames
+    }
+    @IBAction func changeShoppingListPressed(_ sender: UIButton) {
+    }
+    
 }
