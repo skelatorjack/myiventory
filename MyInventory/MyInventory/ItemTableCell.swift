12bf5f118afbc1f4b1c1845cee84c80f5f409352
Jack Pettit <skelatorjack@gmail.com>
2019-04-15 21:44:44 -0500

    Added images to item display. Need to add to ShoppingList.

--- MyInventory/MyInventory/ItemTableCell.swift
+++ MyInventory/MyInventory/ItemTableCell.swift
@@ -14,11 +14,33 @@ class ItemTableCell: UITableViewCell {
     @IBOutlet weak var nameLabel: UILabel!
     @IBOutlet weak var ownerLabel: UILabel!
     @IBOutlet weak var quantityLabel: UILabel!
+    @IBOutlet weak var iconImageView: UIImageView!
     
     
     func decorate(with item: Item?) {
         nameLabel.text = item?.itemName
         ownerLabel.text = item?.itemOwner
         quantityLabel.text = item?.quantityToString()
+        setIcon(using: item)
+    }
+    
+    private func setIcon(using item: Item?) {
+        guard let itemCategory = item?.itemCategory else {
+            return
+        }
+        switch itemCategory {
+        case .Cleaning: setImage(using: "Cleaning")
+        case .Clothes: setImage(using: "Clothing")
+        case .Food: setImage(using: "Food")
+        case .Fashion: setImage(using: "Fashion")
+        case .Tech: setImage(using: "Tech")
+        case .Tools: setImage(using: "Tools")
+        case .Yard: setImage(using: "Yard")
+        default: return
+        }
+    }
+    
+    private func setImage(using fileName: String) {
+        iconImageView.image = UIImage(named: fileName)
     }
 }
