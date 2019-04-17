f46fc9c5b2fb26a7d31a67e675ed77d1c1e22c6b
Jack Pettit <skelatorjack@gmail.com>
2019-04-16 19:22:22 -0500

    Added Item icons to ShoppingList Items.

--- MyInventory/MyInventory/ShoppingListDetailCell.swift
+++ MyInventory/MyInventory/ShoppingListDetailCell.swift
@@ -14,11 +14,30 @@ class ShoppingListDetailCell: UITableViewCell {
     @IBOutlet weak var nameLabel: UILabel!
     @IBOutlet weak var quantityLabel: UILabel!
     @IBOutlet weak var ownerLabel: UILabel!
+    @IBOutlet weak var cellIcon: UIImageView!
     
     
     func decorate(with item: Item) {
         nameLabel.text = item.itemName
         quantityLabel.text = "\(item.itemQuantity)"
         ownerLabel.text = item.itemOwner
+        setIcon(with: item)
+    }
+    
+    private func setIcon(with item: Item) {
+        switch item.itemCategory {
+        case .Cleaning: setImage(using: "Cleaning")
+        case .Clothes: setImage(using: "Clothing")
+        case .Food: setImage(using: "Food")
+        case .Fashion: setImage(using: "Fashion")
+        case .Tech: setImage(using: "Tech")
+        case .Tools: setImage(using: "Tools")
+        case .Yard: setImage(using: "Yard")
+        default: return
+        }
+    }
+    
+    private func setImage(using fileName: String) {
+        cellIcon.image = UIImage(named: fileName)
     }
 }
